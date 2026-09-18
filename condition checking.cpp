d817ec62c09a38631222d38744f099a64931ea5a
Shreya (sia) <shreyashinde3005@gmail.com>
2026-09-18 12:40:25 +0530

    Add files via upload

---
+++ condition checking.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 25, b= 35;
+    if (a>b)
+    {
+        cout<<"a is greater"<<endl;
+    }
+    else
+    {
+        cout<<"b is greater"<<endl;
+    }
+    return 0;
+}
+
